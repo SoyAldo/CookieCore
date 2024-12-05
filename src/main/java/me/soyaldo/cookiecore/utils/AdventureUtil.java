80719bbaa63f3b73067282557f46e2ffe022b596
SoyAldo <pereyraosvaldo@live.com.ar>
2024-12-05 17:03:59 -0300

    [1.2.0] AdventureUtil updated.
    New methods added in the adventure util and fix the null command sender in getComponent method.

--- src/main/java/me/soyaldo/cookiecore/utils/AdventureUtil.java
+++ src/main/java/me/soyaldo/cookiecore/utils/AdventureUtil.java
@@ -2,6 +2,7 @@ package me.soyaldo.cookiecore.utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -17,12 +18,12 @@ public class AdventureUtil {
         return getComponent(text, new String[][]{}, null);
     }
 
-    public static Component getComponent(String text, Player player) {
-        return getComponent(text, new String[][]{}, player);
+    public static Component getComponent(String text, String[][] replacements) {
+        return getComponent(text, replacements, null);
     }
 
-    public static Component getComponent(String text, String[][] replacements, Player player) {
-        return getComponent(text, replacements, (CommandSender) player);
+    public static Component getComponent(String text, CommandSender commandSender) {
+        return getComponent(text, new String[][]{}, commandSender);
     }
 
     public static Component getComponent(String text, String[][] replacements, CommandSender commandSender) {
@@ -30,9 +31,36 @@ public class AdventureUtil {
         // Replacing the replacements on te text
         text = TextUtil.replace(text, replacements);
         // Applying the placeholders
-        if (commandSender instanceof Player) text = PlaceholderUtil.setPlaceholder((Player) commandSender, text);
+        if (commandSender != null) {
+            if (commandSender instanceof Player) text = PlaceholderUtil.setPlaceholder((Player) commandSender, text);
+        }
         // Returning the minimessage component deserialized.
         return MiniMessage.miniMessage().deserialize(text);
+    }
+
+    public static String legacyProcess(String text) {
+        return legacyProcess(getComponent(text));
+    }
+
+    public static String legacyProcess(String text, String[][] replacements) {
+        return legacyProcess(getComponent(text, replacements));
+    }
+
+    public static String legacyProcess(String text, CommandSender commandSender) {
+        return legacyProcess(getComponent(text, new String[][]{}, commandSender));
+    }
+
+    public static String legacyProcess(String text, String[][] replacements, CommandSender commandSender) {
+        return legacyProcess(getComponent(text, replacements, commandSender));
+    }
+
+    public static String legacyProcess(Component component) {
+        return LegacyComponentSerializer.builder()
+                .character('§')
+                .hexColors()
+                .hexCharacter('§')
+                .build()
+                .serialize(component);
     }
 
 }
